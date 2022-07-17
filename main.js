87fafa6f0391ad712ce268b74be8f5135c8aeb43
Ian <iansedano@gmail.com>
2022-07-17 20:27:33 +0200

    starting javascript

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+/*
+
+
+
+
+*/
+
+let turn = "X";
+
+function swapTurns() {
+  if (turn == "X") {
+    turn = "O";
+  } else {
+    turn = "X";
+  }
+}
+
+let boxOne = document.getElementById("1");
+
+boxOne.addEventListener("click", handleClick);
+
+function handleClick(eventObject) {
+  eventObject.target.innerText = "X";
+}
+
+let boxTwo = document.getElementById("2");
+
+boxTwo.addEventListener("click", handleClick2);
+
+function handleClick2(eventObject) {
+  eventObject.target.innerText = "O";
+}
