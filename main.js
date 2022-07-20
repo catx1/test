a835ae00d3f07821352c08ef03b04ba1dd38e017
catherine <catxcurrie@gmail.com>
2022-07-20 19:27:18 +0100

    grid

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('hello world')
+/*
+
+let turn = "x"
+
+function swapTurns() {
+    if (turn == "x") {
+        turn = "0";
+    } else {
+        turn = "x"
+    }
+}
+
+let boxOne = document.getElementById("1");
+
+boxOne.addEventListener("click", handleClick)
+
+function handleClick(eventObject) {
+    eventObject.target.innerText = "x";
+}
+
+let boxTwo = document.getElementById("2");
+
+boxTwo.addEventListener("click", handleClick2)
+
+function handleClick2(eventObject) {
+    eventObject.target.innerText = "o";
+}
+
+*/
