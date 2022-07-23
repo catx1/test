5174eeb356bb00751f700d0710c8e877e4aea310
catherine <catxcurrie@gmail.com>
2022-07-23 19:17:32 +0100

    starting javascript 2

--- main.js
+++ main.js
@@ -1,29 +1,43 @@
-/*
+console.log("hello")
 
 let turn = "x"
 
+
+for (let i = 1; i < 10; i++) {
+
+    let box = document.getElementById(`box${[i]}`);
+
+    console.log(box[i])
+    
+    //box.addEventListener("click", handleClick1)
+    
+    if (turn === "o") {
+    
+        box.addEventListener("click", handleClick1)
+
+        } else {
+
+            box.addEventListener("click", handleClick2)
+        }
+
+    turn = swapTurns(turn)
+
+    }
+
+
+function handleClick1(eventObject) {
+    eventObject.target.innerText = "o";
+}
+
+function handleClick2(eventObject) {
+    eventObject.target.innerText = "x";
+}
+
 function swapTurns() {
     if (turn == "x") {
-        turn = "0";
+        turn = "o";
     } else {
         turn = "x"
     }
 }
 
-let boxOne = document.getElementById("1");
-
-boxOne.addEventListener("click", handleClick)
-
-function handleClick(eventObject) {
-    eventObject.target.innerText = "x";
-}
-
-let boxTwo = document.getElementById("2");
-
-boxTwo.addEventListener("click", handleClick2)
-
-function handleClick2(eventObject) {
-    eventObject.target.innerText = "o";
-}
-
-*/
