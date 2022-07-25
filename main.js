57adf227748a59cba8109f3ab6016ba4561689fc
catherine <catxcurrie@gmail.com>
2022-07-25 13:22:04 +0100

    main game

--- main.js
+++ main.js
@@ -1,95 +1,191 @@
 
 let turn = "x"
 
-win = false
-//winCombo = false
+let win = false
 
 document.getElementById("headerbanner").innerHTML = "press a box to start";
 
-do {
-    for (let i = 1; i < 10; i++) {
+for (let i = 1; i < 10; i++) {
 
-        let box = document.getElementById(`box${[i]}`);
-        box.addEventListener("click", handleClick)
+    let box = document.getElementById(`box${[i]}`);
+    box.addEventListener("click", handleClick)
 
-        }
-
-
-    function handleClick(eventObject) {
-        let id = eventObject.target.id;
-        console.log(id)
-        let box = eventObject.target;
-
-        box.innerText = turn;
-        swapTurns()
-        checkWin()
-        //checkCombinations()
     }
+
+
+function handleClick(eventObject) {
+    let id = eventObject.target.id;
+    let box = eventObject.target;
+
+    box.innerText = turn;
+    swapTurns()
+    checkCombinations()
+    checkWin()
 }
-while (win = false);
+
 
 function swapTurns() {
     if (turn == "x") {
         turn = "o";
-        document.getElementById("headerbanner").innerHTML = "player o's turn";
+        document.getElementById("headerbanner").innerText = "player o's turn";
     } else {
         turn = "x"
-        document.getElementById("headerbanner").innerHTML = "player x's turn";
+        document.getElementById("headerbanner").innerText = "player x's turn";
     }
 }
 
-//function checkCombinations(winCombo) {
-    //return 
-let combination1 = false
-if (document.getElementById("box1").value === document.getElementById("box2").value === document.getElementById("box3").value) {
-    combination1 = true
-}
+let [
+    combination1,
+    combination2,
+    combination3,
+    combination4,
+    combination5,
+    combination6,
+    combination7,
+    combination8,
+  ] = [false, false, false, false, false, false, false, false];
+  
 
-let combination2 = false
-if (document.getElementById("box4").value === document.getElementById("box5").value === document.getElementById("box6").value) {
-    combination2 = true
-}
+function checkCombinations() {
+    if (
+      document.getElementById("box1").innerText ===
+        document.getElementById("box2").innerText &&
+      document.getElementById("box2").innerText ===
+        document.getElementById("box3").innerText  &&
+        document.getElementById("box1").innerText != "" &&
+        document.getElementById("box2").innerText != "" &&
+        document.getElementById("box3").innerText != ""  
+    ) {
+      combination1 = true;
+    }
+  
+    if (
+      document.getElementById("box4").innerText ===
+        document.getElementById("box5").innerText &&
+      document.getElementById("box5").innerText ===
+        document.getElementById("box6").innerText && 
+        document.getElementById("box4").innerText != "" &&
+        document.getElementById("box5").innerText != "" &&
+        document.getElementById("box6").innerText != ""  
+    ) {
+      combination2 = true;
+    }
+  
+    if (
+      document.getElementById("box7").innerText ===
+        document.getElementById("box8").innerText &&
+      document.getElementById("box8").innerText ===
+        document.getElementById("box9").innerText && 
+        document.getElementById("box7").innerText != "" &&
+        document.getElementById("box8").innerText != "" &&
+        document.getElementById("box9").innerText != ""  
+    ) {
+      combination3 = true;
+    }
+  
+    if (
+        document.getElementById("box2").innerText ===
+          document.getElementById("box5").innerText &&
+        document.getElementById("box5").innerText ===
+          document.getElementById("box8").innerText && 
+          document.getElementById("box2").innerText != "" &&
+          document.getElementById("box5").innerText != "" &&
+          document.getElementById("box8").innerText != "" 
+      
+      ) {
+        combination4 = true;
+      }
 
-let combination3 = false
-if (document.getElementById("box7").value === document.getElementById("box8").value === document.getElementById("box9").value) {
-    combination3 = true 
-}
-
-let combination4 = false
-if (document.getElementById("box1").value === document.getElementById("box4").value === document.getElementById("box7").value) {
-combination4 = true
-}
-
-let combination5 = false
-if (document.getElementById("box2").value === document.getElementById("box5").value === document.getElementById("box8").value) {
-    combination5 = true
-}
-
-let combination6 = false
-if (document.getElementById("box3").value === document.getElementById("box6").value === document.getElementById("box9").value) {
-    combination6 = true
-}
-
-let combination7 = false
-if (document.getElementById("box1").value === document.getElementById("box5").value === document.getElementById("box9").value) {
-    combination7 = true
-}
-
-let combination8 = false
-if (document.getElementById("box3").value === document.getElementById("box5").value === document.getElementById("box7").value) {
-    combination8 = true
-}
+    if (
+      document.getElementById("box1").innerText ===
+        document.getElementById("box4").innerText &&
+      document.getElementById("box4").innerText ===
+        document.getElementById("box7").innerText && 
+        document.getElementById("box1").innerText != "" &&
+        document.getElementById("box4").innerText != "" &&
+        document.getElementById("box7").innerText != "" 
+    
+    ) {
+      combination5 = true;
+    }
+  
+    if (
+      document.getElementById("box3").innerText ===
+        document.getElementById("box6").innerText &&
+      document.getElementById("box6").innerText ===
+        document.getElementById("box9").innerText && 
+        document.getElementById("box3").innerText != "" &&
+        document.getElementById("box6").innerText != "" &&
+        document.getElementById("box9").innerText != ""  
+    ) {
+      combination6 = true;
+    }
+  
+    if (
+      document.getElementById("box1").innerText ===
+        document.getElementById("box5").innerText &&
+      document.getElementById("box5").innerText ===
+        document.getElementById("box9").innerText && 
+        document.getElementById("box1").innerText != "" &&
+        document.getElementById("box5").innerText != "" &&
+        document.getElementById("box9").innerText != ""  
+    ) {
+      combination7 = true;
+    } 
+    
+  
+    if (
+      document.getElementById("box3").innerText ===
+        document.getElementById("box5").innerText &&
+      document.getElementById("box5").innerText ===
+        document.getElementById("box7").innerText && 
+        document.getElementById("box3").innerText != "" &&
+        document.getElementById("box5").innerText != "" &&
+        document.getElementById("box7").innerText != ""  
+    ) {
+      combination8 = true;
+    }
+  }
 
 
 
 function checkWin() {
-    if (combination1 || combination2 || combination3 || combination4 ||combination5 || combination6 || combination7 || combination8) {
+    if (
+        combination1 || 
+        combination2 || 
+        combination3 || 
+        combination4 ||
+        combination5 || 
+        combination6 || 
+        combination7 || 
+        combination8
+    ) {
         win = true
-        console.log(win)
+        console.log("done")
+
+        // display winner 
+        if (turn === "x") {
+            turn = "o"
+        } else {
+            turn = "x";
+        }
+        document.getElementById("headerbanner").innerText = "player "+turn+" has won!";
+        for (let i = 1; i < 10; i++) {
+
+            let box = document.getElementById(`box${[i]}`);
+            box.removeEventListener("click", handleClick)
+        
+            }
+       
+    } else {
+        console.log("not done")
     }
 } 
     
 
-if (win = true) {
-    document.getElementById("headerbanner").innerHTML = "you won!";
+const button = document.querySelector("button")
+button.onclick = () => {
+    window.location.reload();
 }
+
+//function draw()
